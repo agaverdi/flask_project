fdfad907a71e78df8db1f38a62f80325d87aca4c
agaverdi <adil.agaverdiyev@mail.ru>
2020-08-06 04:46:41 +0400

    project

---
+++ app/answers.py
@@ -0,0 +1,16 @@
+from app import db
+from app.forms import RegistrationForm
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
